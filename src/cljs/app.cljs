74abef1ec0be61a720c19f8aacf5eb684cb903c8
Geoff Lane <geoff@zorched.net>
2014-02-04 15:04:27 -0500

    Support both current time and converting arbitrary times.
    Add Bootstrap and Angular UI for better style and UX.
    
    Signed-off-by: Geoff Lane <geoff@zorched.net>

--- src/cljs/app.cljs
+++ src/cljs/app.cljs
@@ -5,12 +5,13 @@
                [purnam.angular :only [def.module def.config def.factory
                                       def.controller def.service]]))
 
-(def.module tzoneApp [ngRoute ngResource])
+(def.module tzoneApp [ngRoute ngResource ui.bootstrap])
 
 (def.config tzoneApp [$routeProvider]
   (doto $routeProvider
-    (.otherwise (obj :templateUrl "partials/tzone.html"
-                     :controller "TzoneCtrl"))))
+    (.when "/current" (obj :templateUrl "partials/current.html" :controller "CurrentCtrl"))
+    (.when "/convert" (obj :templateUrl "partials/convert.html" :controller "ConvertCtrl"))
+    (.otherwise (obj :redirecTo "/current"))))
 
 (def.factory tzoneApp.TZone [$resource]
   ($resource "tzone"))
@@ -19,14 +20,19 @@
   (obj :current (fn [to key] ($http.get (str "/convertCurrent?to=" to "&key=" key)))
        :other (fn [from to time key] ($http.get (str "/convertTime?to=" to "&from=" from "&time=" time "&key=" key)))))
 
-(def.controller tzoneApp.TzoneCtrl [$scope TZone Convert]
+(def.controller tzoneApp.CurrentCtrl [$scope TZone Convert]
   (! $scope.tzones (TZone.query))
+  (! $scope.currentTime (obj :time (js/Date.) :tz "Local Time" ))
+  (! $scope.convert (fn [totz]
+                      (->
+                        (Convert.current totz "xxx")
+                        (.success (fn [data] (! $scope.currentTime data)))))))
 
-  ($scope.$watch "tzone" (fn [tzone]
-                           (if (nil? tzone)
-                             (! $scope.currentTime (obj :time (js/Date.) :tz "Local Time" ))
-                             (->
-                               (Convert.current tzone "xxx")
-                               (.success (fn [data]
-                                           (js/console.log data)
-                                           (! $scope.currentTime data))))))))
+(def.controller tzoneApp.ConvertCtrl [$scope TZone Convert dateFilter]
+  (! $scope.tzones (TZone.query))
+  (! $scope.currentTime (obj :time (js/Date.) :tz "Local Time" ))
+  (! $scope.time (dateFilter (js/Date.) "yyyy-MM-dd'T'hh:mm:ss.sss0"))
+  (! $scope.convert (fn [fromtz totz time]
+                      (->
+                        (Convert.other fromtz totz time "xxx")
+                        (.success (fn [data] (! $scope.currentTime data)))))))
